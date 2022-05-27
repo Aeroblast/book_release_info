88d8cde5c28cc74ded27878e78ca80eaf52f3dec
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2022-05-27 10:40:56 +0800

    Update dataSource.js

--- src/dataSource.js
+++ src/dataSource.js
@@ -38,7 +38,7 @@ async function GetData(filename) {
 
 
     result.sort((a, b) => a.records[0].date.localeCompare(b.records[0].date));
-    console.log(filename+":")
+    console.log(filename + ":")
     console.log(result)
     return result;
 }
@@ -57,56 +57,58 @@ function Title2Key(rawTitle) {
     r = r.replace(/[\uFF10-\uFF19]/g, m => String.fromCharCode(m.charCodeAt(0) - 0xfee0)); //fullwidth number
     r = r.replace(/[\u2460-\u2473]/g, m => String.fromCharCode(m.charCodeAt(0) - 0x2460 + 49))//⓪
     r = r.replace(/[(（][^0-9]*?[）)]/g, "");//去除括号内非数字的，比如（仮）
-    
+
 
     r = r.replace(/[(（]([0-9]*?)[）)]/g, m => m[1]); // (1) => 1
     r = r.replace(/[0-9]+/g, m => parseInt(m)); // 01 => 1
-    
+
     r = r.replaceAll('\u3000', '').replaceAll(' ', '');
     r = r.trim();
     return r;
 }
 
-const sourcePriorityMap = {
+const sourceBuffMap = {
     "https://www.ktcom.jp/new_release.htm": 1,
     "https://bookclub.kodansha.co.jp/calendar": -1,
     "https://kc.kodansha.co.jp/calendar": -1,
     "https://dengekibunko.jp/product/newrelease-bunko.html": -1,
     "https://gagagabunko.jp/release/index.html": -1,
-    "https://fantasiabunko.jp/":-1,
+    "https://fantasiabunko.jp/": -1,
     "手書き": 0
 }
-const sourcePriorityMap2 = {
+const sourceBuffMap2 = {
     "hon-hikidashi.jp": -10,
-    "www.amazon.co.jp": 5,
+    "www.amazon.co.jp": 1,
 
 }
 /**
  * 
- * @returns {Number} priorty
+ * @returns {Number} buff (days)
  */
-function getSourcePriorty(record) {
+function getSourceBuff(record) {
     const src = record.source;
-    if (src in sourcePriorityMap) {
-        return sourcePriorityMap[src];
+    if (src in sourceBuffMap) {
+        return sourceBuffMap[src];
     }
     let x = src.split("/");
     if (x.length < 3) return 0;
     const host = x[2];
-    
-    if (host in sourcePriorityMap2) {
-        return sourcePriorityMap2[host];
+
+    if (host in sourceBuffMap2) {
+        return sourceBuffMap2[host];
     }
     return 0;
 }
 
 function CompareRecords(a, b) {
-    const src_p_a = getSourcePriorty(a), src_p_b = getSourcePriorty(b);
-    if (src_p_a != src_p_b) {
-        return src_p_b - src_p_a;
-    }
-
-    return -a.recordTime.localeCompare(b.recordTime);
+    if (a.coverFile && !b.coverFile) return -1;
+    if ((!a.coverFile) && b.coverFile) return 1;
+    const src_buff_a = getSourceBuff(a), src_buff_b = getSourceBuff(b);
+    let time_a = new Date(a.recordTime), time_b = new Date(b.recordTime);
+    time_a.setDate(time_a.getDate() + src_buff_a);
+    time_b.setDate(time_b.getDate() + src_buff_b);
+    let r = time_b.getTime() - time_a.getTime();
+    return r
 }
 exports.GetData = GetData;
 exports.CompareRecords = CompareRecords
